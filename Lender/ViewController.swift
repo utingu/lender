a444801b01be572b05a762c37d20d175dfa5dd1f
Alex Chen <alexmchen@gmail.com>
2017-03-31 14:38:25 -0500

    Initial Commit

---
+++ Lender/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lender
+//
+//  Created by Alex Chen on 3/31/17.
+//  Copyright © 2017 Alex Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
